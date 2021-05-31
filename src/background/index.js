3c57dacd332b38c1a852c72db7fae81c6905c369
nisarg0 <nisarggogate212@gmail.com>
2021-06-01 02:12:33 +0530

    working on localForage

--- src/background/index.js
+++ src/background/index.js
@@ -1,9 +1,117 @@
-// If your extension doesn't need a background script, just leave this file empty
+// import localforage from "localforage";
 
-messageInBackground();
+// console.log("IN background");
+// var curr_index = {
+// 	currrent_index: 8,
+// };
+// var myContests = [];
+// // var platforms = [
+// // 	{ code_chef: true },
+// // 	{ codeforces: true },
+// // 	{ leet_code: true },
+// // 	{ at_coder: true },
+// // 	{ hacker_earth: true },
+// // 	{ kick_start: true },
+// // 	{ top_coder: true },
+// // ];
 
-// This needs to be an export due to typescript implementation limitation of needing '--isolatedModules' tsconfig
-export function messageInBackground() {
-  console.log('I can run your javascript like any other code in your project');
-  console.log('just do not forget, I cannot render anything !');
-}
+// // var platforms = {
+// // 	code_chef: true,
+// // 	codeforces: true,
+// // 	leet_code: true,
+// // 	at_coder: true,
+// // 	hacker_earth: true,
+// // 	kick_start: true,
+// // 	top_coder: true,
+// // };
+
+// var platforms = [
+// 	{
+// 		platform: "code_chef",
+// 		isSubscribed: true,
+// 	},
+// 	{
+// 		platform: "codeforces",
+// 		isSubscribed: true,
+// 	},
+// 	{
+// 		platform: "leet_code",
+// 		isSubscribed: true,
+// 	},
+// 	{
+// 		platform: "at_coder",
+// 		isSubscribed: true,
+// 	},
+// 	{
+// 		platform: "hacker_earth",
+// 		isSubscribed: true,
+// 	},
+// 	{
+// 		platform: "kick_start",
+// 		isSubscribed: true,
+// 	},
+// 	{
+// 		platform: "top_coder",
+// 		isSubscribed: true,
+// 	},
+// ];
+
+// localforage.setItem("platforms", platforms);
+
+// async function getAllMyContests() {
+// 	for (var i = 0; i < platforms.length; i++) {
+// 		if (!platforms[i].isSubscribed) continue;
+
+// 		var contests = await fetchContestDetails(platforms[i].platform);
+// 		for (var contest of contests) {
+// 			myContests.push(contest);
+// 		}
+// 	}
+// 	console.log(myContests);
+// 	await localforage.setItem("myContests", myContests);
+// }
+
+// getAllMyContests();
+
+// async function fetchContestDetails(platform) {
+// 	const res = await fetch(`https://www.kontests.net/api/v1/${platform}`, {
+// 		method: "GET",
+// 		headers: {
+// 			"Content-type": "application/json; charset=UTF-8",
+// 		},
+// 	});
+
+// 	if (!res.ok) {
+// 		const message = "An error has occured";
+// 		throw new Error(message);
+// 	}
+
+// 	const contestDetails = await res.json();
+// 	return contestDetails;
+// }
+
+// // =============================Alarm every 24 hours =======================
+// chrome.alarms.create("updateUpcomingContestDetails", {
+// 	//options
+// 	when: Date.now(),
+// 	periodInMinutes: 1440,
+// });
+
+// chrome.alarms.onAlarm.addListener((alarm) => {
+// 	if (alarm.name === "updateUpcomingContestDetails") {
+// 		// console.log("alarm1 running");
+// 		getAllMyContests().then(() => {
+// 			storemyContestInChromeDB();
+// 		});
+
+// 		localforage.getItem("platforms", function (err, result) {
+// 			// console.log(result);
+// 			if (result.userPlatforms === undefined) {
+// 				// console.log("result was undefined");
+// 				storePlatformsInChromeDB();
+// 			} else {
+// 				console.log("It was defined");
+// 			}
+// 		});
+// 	}
+// });
--- src/components/MyContest.js
+++ src/components/MyContest.js
@@ -1,36 +1,33 @@
-import axios from 'axios'
-import React,{useState,useEffect} from 'react'
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import localforage from "localforage";
 
 export default function MyContest() {
-    const [contests, setcontests] = useState([])
-    useEffect(function(){
-        axios
-        .get("https://www.kontests.net/api/v1/all")
-        .then((response)=>setcontests(response.data))
-        .then(console.log("error"))
+	const [contests, setcontests] = useState([]);
+	useEffect(function () {
+		axios
+			.get("https://www.kontests.net/api/v1/all")
+			.then((response) => setcontests(response.data))
+			.then(console.log("error"));
+	});
 
-    })
-   
-    {
-        return (
-            <div>
-                
-            {contests.map((contest) =>  (
-                <div class="card text-center">
-                <div class="card-header">
-                  {contest.name}
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">Start:{contest.start_time}</h5>
-                  <a href="{contest.url}" class="btn btn-primary">Go to Contest</a>
-                </div>
-              </div>
-                        
-            ))}
-        
-                
-            </div>
-    )
-    }
+	{
+		return (
+			<div>
+				{contests.map((contest) => (
+					<div class="card text-center">
+						<div class="card-header">{contest.name}</div>
+						<div class="card-body">
+							<h5 class="card-title">
+								Start:{contest.start_time}
+							</h5>
+							<a href="{contest.url}" class="btn btn-primary">
+								Go to Contest
+							</a>
+						</div>
+					</div>
+				))}
+			</div>
+		);
+	}
 }
