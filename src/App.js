4928c6a4d80d23bbfd772570cdcee3f768953f4d
nisarg0 <nisarggogate212@gmail.com>
2021-06-01 21:11:55 +0530

    DB setup in background step 1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import './App.css';
-import React, { Component } from 'react'
-import Subscribe from './components/Subscribe'
-import NavigationBar from './components/NavigationBar'
-import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
-import MyContest from './components/MyContest'
-
+import "./App.css";
+import React, { Component } from "react";
+import Subscribe from "./components/Subscribe";
+import NavigationBar from "./components/NavigationBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MyContest from "./components/MyContest";
 
 function App() {
-
-  return (
-    
-    <div>
-      <Router>
-        <NavigationBar/>
-        <Switch>
-          <Route path='/' exact component={()=><MyContest/>} />
-          <Route path='/Subscribe' exact component={()=><Subscribe/>} />
-        </Switch>
-      </Router>
-    </div>
-    
-  );
-  
+	return (
+		<div>
+			<Router>
+				<NavigationBar />
+				<Switch>
+					<Route path="/" exact component={() => <MyContest />} />
+					<Route
+						path="/Subscribe"
+						exact
+						component={() => <Subscribe />}
+					/>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- src/background/index.js
+++ src/background/index.js
@@ -1,117 +1,137 @@
-// import localforage from "localforage";
+import localforage from "localforage";
+// https://levelup.gitconnected.com/how-to-use-background-script-to-fetch-data-in-chrome-extension-ef9d7f69625d
+console.log("IN background");
 
-// console.log("IN background");
-// var curr_index = {
-// 	currrent_index: 8,
-// };
-// var myContests = [];
-// // var platforms = [
-// // 	{ code_chef: true },
-// // 	{ codeforces: true },
-// // 	{ leet_code: true },
-// // 	{ at_coder: true },
-// // 	{ hacker_earth: true },
-// // 	{ kick_start: true },
-// // 	{ top_coder: true },
-// // ];
+var myContests = [];
 
-// // var platforms = {
-// // 	code_chef: true,
-// // 	codeforces: true,
-// // 	leet_code: true,
-// // 	at_coder: true,
-// // 	hacker_earth: true,
-// // 	kick_start: true,
-// // 	top_coder: true,
-// // };
+var platforms = [
+	{
+		platform: "code_chef",
+		isSubscribed: true,
+	},
+	{
+		platform: "codeforces",
+		isSubscribed: true,
+	},
+	{
+		platform: "leet_code",
+		isSubscribed: true,
+	},
+	{
+		platform: "at_coder",
+		isSubscribed: true,
+	},
+	{
+		platform: "hacker_earth",
+		isSubscribed: true,
+	},
+	{
+		platform: "kick_start",
+		isSubscribed: true,
+	},
+	{
+		platform: "top_coder",
+		isSubscribed: true,
+	},
+];
 
-// var platforms = [
-// 	{
-// 		platform: "code_chef",
-// 		isSubscribed: true,
-// 	},
-// 	{
-// 		platform: "codeforces",
-// 		isSubscribed: true,
-// 	},
-// 	{
-// 		platform: "leet_code",
-// 		isSubscribed: true,
-// 	},
-// 	{
-// 		platform: "at_coder",
-// 		isSubscribed: true,
-// 	},
-// 	{
-// 		platform: "hacker_earth",
-// 		isSubscribed: true,
-// 	},
-// 	{
-// 		platform: "kick_start",
-// 		isSubscribed: true,
-// 	},
-// 	{
-// 		platform: "top_coder",
-// 		isSubscribed: true,
-// 	},
-// ];
+// Fetch Function
+async function fetchAllMyContests() {
+	console.log("In fetch all my contests");
+	myContests = [];
+	for (var i = 0; i < platforms.length; i++) {
+		if (!platforms[i].isSubscribed) continue;
 
-// localforage.setItem("platforms", platforms);
+		var contests = await fetchContestDetails(platforms[i].platform);
+		for (var contest of contests) {
+			myContests.push(contest);
+		}
+	}
+}
 
-// async function getAllMyContests() {
-// 	for (var i = 0; i < platforms.length; i++) {
-// 		if (!platforms[i].isSubscribed) continue;
+// =============================Alarms, startup, instaleed functions =======================
 
-// 		var contests = await fetchContestDetails(platforms[i].platform);
-// 		for (var contest of contests) {
-// 			myContests.push(contest);
-// 		}
-// 	}
-// 	console.log(myContests);
-// 	await localforage.setItem("myContests", myContests);
-// }
+// create alarm for fresh on installed/updated, and start fetch data
+chrome.runtime.onInstalled.addListener(() => {
+	console.log("onInstalled....");
+	scheduleRequest();
+	startRequest();
+});
 
-// getAllMyContests();
+// fetch and save data when chrome restarted, alarm will continue running when chrome is restarted
+chrome.runtime.onStartup.addListener(() => {
+	console.log("onStartup....");
+	startRequest();
+});
 
-// async function fetchContestDetails(platform) {
-// 	const res = await fetch(`https://www.kontests.net/api/v1/${platform}`, {
-// 		method: "GET",
-// 		headers: {
-// 			"Content-type": "application/json; charset=UTF-8",
-// 		},
-// 	});
+// schedule a new fetch every 1440 minutes
+function scheduleRequest() {
+	console.log("schedule refresh alarm to 1440 minutes...");
+	chrome.alarms.create("refresh", { periodInMinutes: 1440 });
+}
 
-// 	if (!res.ok) {
-// 		const message = "An error has occured";
-// 		throw new Error(message);
-// 	}
+// fetch data and save to local storage
+async function startRequest() {
+	console.log("start HTTP Request...");
+	// We need to get the array that user has stored previously if not then we use original one
+	await getPlatforms();
+	await fetchAllMyContests();
+	await setmyContests();
+	await getmyContests();
+}
 
-// 	const contestDetails = await res.json();
-// 	return contestDetails;
-// }
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+	if (alarm.name === "refresh") {
+		await getPlatforms();
+		await fetchAllMyContests();
+		await setmyContests();
+	}
+});
 
-// // =============================Alarm every 24 hours =======================
-// chrome.alarms.create("updateUpcomingContestDetails", {
-// 	//options
-// 	when: Date.now(),
-// 	periodInMinutes: 1440,
-// });
+// ========================================= DB ===================================================
+async function setmyContests() {
+	console.log("In setmyContests");
+	await localforage.setItem("myContests", myContests);
+}
 
-// chrome.alarms.onAlarm.addListener((alarm) => {
-// 	if (alarm.name === "updateUpcomingContestDetails") {
-// 		// console.log("alarm1 running");
-// 		getAllMyContests().then(() => {
-// 			storemyContestInChromeDB();
-// 		});
+async function setmyPlatforms() {
+	console.log("In setmyPlatforms");
+	await localforage.setItem("platforms", myContests);
+}
 
-// 		localforage.getItem("platforms", function (err, result) {
-// 			// console.log(result);
-// 			if (result.userPlatforms === undefined) {
-// 				// console.log("result was undefined");
-// 				storePlatformsInChromeDB();
-// 			} else {
-// 				console.log("It was defined");
-// 			}
-// 		});
-// 	}
-// });
+function getmyContests() {
+	console.log("In getmyContests");
+	localforage.getItem("myContests", function (err, value) {
+		if (err) throw err;
+		myContests = value;
+	});
+}
+async function getPlatforms() {
+	console.log("1. In getPlatforms");
+	await localforage.getItem("platforms", function (err, value) {
+		if (err || value === null) {
+			console.log("Err: No platforms array in DB");
+			return;
+		}
+		myContests = value;
+		console.log(value);
+	});
+}
+
+// ========================================== Helper ==================================================
+
+async function fetchContestDetails(platform) {
+	const res = await fetch(`https://www.kontests.net/api/v1/${platform}`, {
+		method: "GET",
+		headers: {
+			"Content-type": "application/json; charset=UTF-8",
+		},
+	});
+	if (!res.ok) {
+		const message = "An error has occured";
+		throw new Error(message);
+	}
+
+	const contestDetails = await res.json();
+	return contestDetails;
+}
