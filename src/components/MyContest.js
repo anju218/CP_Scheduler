1fca733f726824b76846b181406397544f2871a2
Anjali <anjalirengade23@gmail.com>
2021-06-01 23:16:14 +0530

    commit

--- src/components/MyContest.js
+++ src/components/MyContest.js
@@ -1,3 +1,4 @@
+import { render } from '@testing-library/react'
 import axios from 'axios'
 import React,{useState,useEffect} from 'react'
 
@@ -11,26 +12,53 @@ export default function MyContest() {
         .then(console.log("error"))
 
     })
-   
-    {
-        return (
-            <div>
-                
-            {contests.map((contest) =>  (
-                <div class="card text-center">
-                <div class="card-header">
-                  {contest.name}
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">Start:{contest.start_time}</h5>
-                  <a href="{contest.url}" class="btn btn-primary">Go to Contest</a>
-                </div>
-              </div>
-                        
-            ))}
-        
-                
-            </div>
-    )
-    }
-}
+    // let contest24 = contests.map(x => {
+    //   if ({x.in_24_hrs}==="Yes")
+    //       return x; // unchanged
+    // })
+//     render(); {
+//       return (   
+//               {this.contests.in_24_hours == 'Yes'? {contests.name}: null }
+          
+//       )
+//   }
+// }
+    //   contests.map(()=>(
+    //     if (condition) {
+          
+    //     }
+    //   ))
+    //   if(contests.map(()=>(
+    //     contests.in_24_hours=="Yes"
+    //   ))){
+    //     return(
+    //       <div>
+    //         <div class="card text-center">
+    //           <div class="card-header">
+    //             {contests.name}
+    //           </div>
+    //           <div class="card-body">
+    //             <h5 class="card-title">Start:{contests.start_time}</h5>
+    //             <a href="{contest.url}" class="btn btn-primary">Go to Contest</a>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     )
+    //   }
+    
+      
+    // }
+  //   contest24=<Contest/>
+  //   {contest24.map((contest) =>  (
+  //     <div class="card text-center">
+  //     <div class="card-header">
+  //       {contest.name}
+  //     </div>
+  //     <div class="card-body">
+  //       <h5 class="card-title">Start:{contest.start_time}</h5>
+  //       <a href="{contest.url}" class="btn btn-primary">Go to Contest</a>
+  //     </div>
+  //   </div>
+
+  // ))}
+  }   
